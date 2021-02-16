d8d512f45ae8e10579d84e1250b7d1b0b709ed9b
ForceGT <thakkargaurav409@gmail.com>
2021-02-17 00:05:10 +0530

    Add default values

--- Sources/RatingView/RatingViewModel.swift
+++ Sources/RatingView/RatingViewModel.swift
@@ -14,7 +14,7 @@ public class RatingViewModel : ObservableObject {
     /// An array of 5 values with each value giving the progress of the number of stars
     /// The progress value must be between 0.0 and 1.0 and should be given in reverse
     /// The value at index 0 will correspond to the progress value for 5 star rating, 2nd for 4star rating and so on
-    @Published public var progressValues : [Float] = []
+    @Published public var progressValues : [Float] = [0.0,0.0,0.0,0.0,0.0]
     /// Current Rating from the user if any , defaults to 0
     @Published public var userRating: Int = 0
     /// Net rating from the user if any, default to 0.0
