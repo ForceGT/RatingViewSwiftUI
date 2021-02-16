f7ef7663a8963e23ea0c536dffd8e14694c69ee8
ForceGT <thakkargaurav409@gmail.com>
2021-02-17 00:07:53 +0530

    Add more default values

--- Sources/RatingView/RatingViewModel.swift
+++ Sources/RatingView/RatingViewModel.swift
@@ -14,7 +14,7 @@ public class RatingViewModel : ObservableObject {
     /// An array of 5 values with each value giving the progress of the number of stars
     /// The progress value must be between 0.0 and 1.0 and should be given in reverse
     /// The value at index 0 will correspond to the progress value for 5 star rating, 2nd for 4star rating and so on
-    @Published public var progressValues : [Float] = [0.0,0.0,0.0,0.0,0.0]
+    @Published public var progressValues : [Float] = []
     /// Current Rating from the user if any , defaults to 0
     @Published public var userRating: Int = 0
     /// Net rating from the user if any, default to 0.0
@@ -29,7 +29,7 @@ public class RatingViewModel : ObservableObject {
     public var fillColor : Color = Color.accentColor
     
     
-    public init(progressValues : [Float] = [], userRating: Int = 0, netRate: Float = 0.0, offColor: Color = Color.gray, onColor: Color = Color.accentColor, backgroundColor: Color = Color.gray, fillColor: Color = Color.accentColor) {
+    public init(progressValues : [Float] = [0.0,0.0,0.0,0.0,0.0], userRating: Int = 0, netRate: Float = 0.0, offColor: Color = Color.gray, onColor: Color = Color.accentColor, backgroundColor: Color = Color.gray, fillColor: Color = Color.accentColor) {
         self.progressValues = progressValues
         self.userRating = userRating
         self.netRate = netRate
