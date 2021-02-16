2d695d8230e225aeeefe508b468fad92e0444b14
ForceGT <thakkargaurav409@gmail.com>
2021-02-16 21:03:12 +0530

    Testing again

--- Sources/RatingView/RatingView.swift
+++ Sources/RatingView/RatingView.swift
@@ -6,11 +6,11 @@ public struct RatingBarView: View {
     /// An array of 5 values with each value giving the progress of the number of stars
     /// The progress value must be between 0.0 and 1.0 and should be given in reverse
     /// The value at index 0 will correspond to the progress value for 5 star rating, 2nd for 4star rating and so on
-    @State var progressValues : [Float] = [0.0,0.0,0.0,0.0,0.0]
+    @State var progressValues : [Float] = []
     /// Current Rating from the user if any , defaults to 0
     @State var userRating: Int = 0
     /// Net rating from the user if any, default to 0.0
-    @State var netRate : Float = 0.0
+    @State var netRate : Float = 5.0
     /// The label that appears next to the row of stars
     var label : String = ""
     /// The color of the star that will be displayed when not selected, defaults to gray
@@ -18,11 +18,12 @@ public struct RatingBarView: View {
     /// The color of the star when selected, defaults to accentColor
     var onColor = Color.accentColor
     /// The callback to be triggered when the rating is tapped
-    var onRatingTapped : (Int) -> Void = {_ in }
+    var onRatingTapped : (Int) -> Void = {_ in}
     
     
     //MARK: INITIALIZERS
     public init(progressValues: [Float], userRating: Int, netRate: Float, label: String, offColor: Color = Color.gray, onColor: Color = Color.accentColor, onRatingTapped: @escaping (Int) -> Void){
+        
         self.progressValues = progressValues
         self.userRating = userRating
         self.netRate = netRate
